c9a82c86648cf8fd7ad61fafbefd1d8365d5eba5
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2023-01-31 17:34:04 -0300

    Removing unecessary code

--- src/main/java/com/request/api/RequestApplication.java
+++ src/main/java/com/request/api/RequestApplication.java
@@ -3,14 +3,11 @@ package com.request.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class RequestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RequestApplication.class, args);
-
-		System.out.println(new BCryptPasswordEncoder().encode("123123"));
 	}
 }
